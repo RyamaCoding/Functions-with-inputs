522cefc33d7728d528f15a2b4cd0e72250fbd941
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-09-23 20:49:29 +0100

    understanding functions with inputs

---
+++ main.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello, World!")
+    print("Nice to meet you!")
+    print("Enjoy your stay!")
+
+greet()
+
+def greet_with_name(something):
+    print(f"{something}")
+
+greet_with_name("Alice")
