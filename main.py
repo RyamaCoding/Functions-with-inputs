213d7c3a87bd73edad55773223ea097a33d84251
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-09-25 18:58:21 +0100

    finishing casesar cypher

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
+import art 
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = (position + shift_amount) % len(alphabet)
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+def caesar(text, shift_amount, direction):
+    result_text = ""
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = (position - shift_amount) % len(alphabet)
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
+    for letter in text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if direction == "encode":
+                new_position = (position + shift_amount) % len(alphabet)
+            elif direction == "decode":
+                new_position = (position - shift_amount) % len(alphabet)
+            new_letter = alphabet[new_position]
+            result_text += new_letter
+        else:
+            result_text += letter
+    print(f"The {direction}d text is: {result_text}")
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-if direction == 'encode':
-    encrypt(plain_text = text, shift_amount = shift)
-elif direction == 'decode': 
-    decrypt(cipher_text = text, shift_amount = shift)
-else: 
-    print("Invalid input. Please try again.")
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
